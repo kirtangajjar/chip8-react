988b4369257cc38e7f59eec8abdda07a23453106
Kirtan Gajjar <kirtangajjar95@gmail.com>
2018-10-07 16:37:18 +0530

    Add initial screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  pixelX = 64;
+  pixelY = 32;
+  pixelScale = 10;
+
+  componentDidMount() {
+    let canvas = document.getElementById("screen");
+    let context = canvas.getContext("2d");
+    canvas.width = this.pixelX * this.pixelScale;
+    canvas.height = this.pixelY * this.pixelScale;
+
+    context.fillStyle = "black";
+
+    for (let i = 0; i < this.pixelX; i++) {
+      for (let j = 0; j < this.pixelY; j++) {
+        if (j % 2 === i % 2)
+          context.fillRect(
+            i * this.pixelScale,
+            j * this.pixelScale,
+            this.pixelScale,
+            this.pixelScale
+          );
+      }
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <canvas id="screen" />
       </div>
     );
   }
